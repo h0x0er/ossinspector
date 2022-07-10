145631cf3cc7f225e5023d2cb593b839aa420162
h0x0er <jatink843@protonmail.com>
2022-07-10 13:36:11 +0530

    added support for parsing age from policy

--- util.go
+++ util.go
@@ -5,20 +5,60 @@ import (
 	"strings"
 )
 
-type ConstraintType int
+type ConstraintType byte
 
 const (
 	GREATER_THAN ConstraintType = iota
 	LESSER_THAN
+
+	MONTHS_GREATER_THAN
+	MONTHS_LESSER_THAN
+
+	YEARS_GREATER_THAN
+	YEARS_LESSER_THAN
+
+	DAYS_GREATER_THAN
+	DAYS_LESSER_THAN
+
+	NO_CONSTRAINT
 )
 
-func evaluate(val string) (ConstraintType, int) {
+func evaluate(expr string) (ConstraintType, int) {
 
 	var ctype ConstraintType
 	var value int
 
-	if strings.Index(val, "<") == 0 {
-		s := strings.Split(val, "<")[1]
+	// TODO: checking if expr in of age type
+	if isAge(expr) {
+		value = trim(expr)
+		switch expr[len(expr)-1] {
+		case 'd':
+			ctype = DAYS_GREATER_THAN
+			if isLesser(expr) {
+				ctype = DAYS_LESSER_THAN
+			}
+
+		case 'y':
+			ctype = YEARS_GREATER_THAN
+			if isLesser(expr) {
+				ctype = YEARS_LESSER_THAN
+			}
+
+		case 'm':
+			ctype = MONTHS_GREATER_THAN
+			if isLesser(expr) {
+				ctype = MONTHS_LESSER_THAN
+			}
+
+		default:
+
+		}
+
+		return ctype, value
+	}
+
+	if isLesser(expr) {
+		s := strings.Split(expr, "<")[1]
 		num, err := strconv.Atoi(s)
 		if err == nil {
 			ctype = LESSER_THAN
@@ -26,9 +66,8 @@ func evaluate(val string) (ConstraintType, int) {
 
 		}
 	}
-
-	if strings.Index(val, ">") == 0 {
-		s := strings.Split(val, ">")[1]
+	if isGreater(expr) {
+		s := strings.Split(expr, ">")[1]
 		num, err := strconv.Atoi(s)
 		if err == nil {
 			ctype = GREATER_THAN
@@ -37,4 +76,27 @@ func evaluate(val string) (ConstraintType, int) {
 	}
 
 	return ctype, value
+}
+
+func isAge(expr string) bool {
+	last := len(expr) - 1
+	return expr[last] == 'd' || expr[last] == 'y' || expr[last] == 'm'
+}
+func isLesser(expr string) bool {
+	return expr[0] == '<'
+}
+func isGreater(expr string) bool {
+	return expr[0] == '>'
+}
+
+// trim removes suffix and prefix
+// return the middle value, return -1 if failed
+func trim(expr string) int {
+	tail := expr[1:]
+	head := tail[0 : len(tail)-1]
+	num, err := strconv.Atoi(head)
+	if err == nil {
+		return num
+	}
+	return -1
 }
--- util_test.go
+++ util_test.go
@@ -1,6 +1,8 @@
 package ossinspector
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_evaluate(t *testing.T) {
 	type args struct {
@@ -15,6 +17,9 @@ func Test_evaluate(t *testing.T) {
 		// TODO: Add test cases.
 		{name: "test greater", args: args{val: ">100"}, want: GREATER_THAN, want1: 100},
 		{name: "test lesser", args: args{val: "<100"}, want: LESSER_THAN, want1: 100},
+		{name: "month lesser", args: args{val: "<100m"}, want: MONTHS_LESSER_THAN, want1: 100},
+		{name: "year lesser", args: args{val: "<100y"}, want: YEARS_LESSER_THAN, want1: 100},
+		{name: "days greater", args: args{val: ">100d"}, want: DAYS_GREATER_THAN, want1: 100},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -24,6 +29,51 @@ func Test_evaluate(t *testing.T) {
 			}
 			if got1 != tt.want1 {
 				t.Errorf("evaluate() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
+
+func Test_isAge(t *testing.T) {
+	type args struct {
+		expr string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{name: "test for days", args: args{expr: ">10d"}, want: true},
+		{name: "test for years", args: args{expr: ">10y"}, want: true},
+		{name: "test for months", args: args{expr: ">10m"}, want: true},
+		{name: "test for days", args: args{expr: ">10a"}, want: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isAge(tt.args.expr); got != tt.want {
+				t.Errorf("isAge() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_trim(t *testing.T) {
+	type args struct {
+		expr string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{name: "Extract from month", args: args{expr: "<10m"}, want: 10},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := trim(tt.args.expr); got != tt.want {
+				t.Errorf("trim() = %v, want %v", got, tt.want)
 			}
 		})
 	}
