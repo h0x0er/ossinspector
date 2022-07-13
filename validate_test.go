e8ed21e14ff8b7f82dc9468859723f94f027ae9d
h0x0er <jatink843@protonmail.com>
2022-07-13 19:31:25 +0530

    fixed test

--- validate_test.go
+++ validate_test.go
@@ -27,7 +27,7 @@ func Test_checkExpr(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := checkExpr(tt.args.checkString, tt.args.value); got != tt.want {
+			if got, _ := checkExpr(tt.args.checkString, tt.args.value); got != tt.want {
 				t.Errorf("checkExpr() = %v, want %v", got, tt.want)
 			}
 		})
