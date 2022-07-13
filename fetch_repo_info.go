f8a2c0bb6813894108b76c91461a1f97fd6c9879
h0x0er <jatink843@protonmail.com>
2022-07-13 19:27:25 +0530

    fetching contributors count

--- fetch_repo_info.go
+++ fetch_repo_info.go
@@ -3,8 +3,6 @@ package ossinspector
 import (
 	"context"
 	"errors"
-	"fmt"
-	"log"
 	"os"
 
 	"github.com/google/go-github/v45/github"
@@ -13,15 +11,16 @@ import (
 
 // RepoInfo info about the target repo
 type RepoInfo struct {
-	CreatedAt     int64 // timestamp of creation
-	LastPushedAt  int64
-	LastUpdatedAt int64
-	IsArchived    bool
-	IsDisabled    bool
-	IsForked      bool
-	StaggersCount uint
-	WatcherCount  uint
-	ForkCount     uint
+	CreatedAt         int64 // timestamp of creation
+	LastPushedAt      int64
+	LastUpdatedAt     int64
+	IsArchived        bool
+	IsDisabled        bool
+	IsForked          bool
+	StaggersCount     uint
+	WatcherCount      uint
+	ForkCount         uint
+	ContributorsCount uint
 
 	OwnerInfo   OwnerInfo
 	CommitInfo  CommitInfo
@@ -52,9 +51,15 @@ type ReleaseInfo struct {
 func FetchRepoInfo(owner, repo string) (*RepoInfo, error) {
 	client := getClient()
 	repoInfo := new(RepoInfo)
-
-	addRepoInfo(client, owner, repo, repoInfo)
-	addOwnerInfo(client, owner, repoInfo)
+	var err error
+	err = addRepoInfo(client, owner, repo, repoInfo)
+	if err != nil {
+		return nil, err
+	}
+	err = addOwnerInfo(client, owner, repoInfo)	
+	if err != nil {
+		return nil, err
+	}
 
 	return repoInfo, nil
 }
@@ -62,16 +67,14 @@ func addRepoInfo(client *github.Client, owner, repo string, repoInfo *RepoInfo) 
 
 	repos, resp, err := client.Repositories.Get(context.Background(), owner, repo)
 	if err != nil {
-		log.Printf("Unable to fetch package: %s/%s\n %v", owner, repo, err)
+		logger.Printf("Unable to fetch package: %s/%s\n %v", owner, repo, err)
 		return err
 	}
 
 	if resp.StatusCode != 200 {
-		log.Printf("it seems %s/%s doesn't exists", owner, repo)
+		logger.Printf("it seems %s/%s doesn't exists", owner, repo)
 		return errors.New("repo doesn't exists")
 	}
-	// NOTE: unable to fetch total contributor
-	// contrib, _, err := client.Repositories.ListContributors(context.Background(), owner, repo, nil)
 
 	repoInfo.CreatedAt = repos.GetCreatedAt().Unix()
 	repoInfo.LastUpdatedAt = repos.GetUpdatedAt().Unix()
@@ -84,21 +87,21 @@ func addRepoInfo(client *github.Client, owner, repo string, repoInfo *RepoInfo) 
 	repoInfo.ForkCount = uint(repos.GetForksCount())
 	repoInfo.StaggersCount = uint(repos.GetStargazersCount())
 	repoInfo.WatcherCount = uint(repos.GetStargazersCount())
+	repoInfo.ContributorsCount = uint(getContributorsCount(client, owner, repo))
 
 	return nil
 }
 func addOwnerInfo(client *github.Client, owner string, repoInfo *RepoInfo) error {
 	owner_info, _, err := client.Users.Get(context.Background(), owner)
 	if err != nil {
-		log.Printf("Unable to fetch owner %s", owner)
+		logger.Printf("Unable to fetch owner %s", owner)
 		return err
 	}
 	repoInfo.OwnerInfo.CreatedAt = owner_info.GetCreatedAt().Unix()
 	repoInfo.OwnerInfo.UpdatedAt = owner_info.GetUpdatedAt().Unix()
 	repoInfo.OwnerInfo.ReposCount = owner_info.GetPublicRepos()
-	//TODO: add more owner info
 	repoInfo.OwnerInfo.FollowersCount = getFollowerCounts(client, owner)
-	fmt.Printf("owner_info.GetFollowers(): %v\n", repoInfo.OwnerInfo.FollowersCount)
+	// TODO: need to add total contribution counts of owner on github
 	return nil
 }
 
@@ -106,12 +109,12 @@ func addReleaseInfo(client *github.Client, owner, repo string, repoInfo *RepoInf
 
 	_, resp, err := client.Repositories.GetLatestRelease(context.Background(), owner, repo)
 	if err != nil {
-		log.Printf("Unable to fetch package: %s/%s\n %v", owner, repo, err)
+		logger.Printf("Unable to fetch package: %s/%s\n %v", owner, repo, err)
 		return err
 	}
 
 	if resp.StatusCode != 200 {
-		log.Printf("it seems %s/%s doesn't exists", owner, repo)
+		logger.Printf("it seems %s/%s doesn't exists", owner, repo)
 		return errors.New("repo doesn't exists")
 	}
 
@@ -120,12 +123,13 @@ func addReleaseInfo(client *github.Client, owner, repo string, repoInfo *RepoInf
 func getClient() *github.Client {
 	token := os.Getenv("gh_token")
 	if token != "" {
-		log.Printf("oauth token found")
+		logger.Printf("gh_token environment variable found")
 		ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
 		tc := oauth2.NewClient(context.Background(), ts)
 		client := github.NewClient(tc)
 		return client
 	}
+	logger.Printf("gh_token environment variable not found. Setup gh_token to evade rate limiting")
 	client := github.NewClient(nil)
 	return client
 
@@ -136,6 +140,10 @@ func getFollowerCounts(client *github.Client, user string) int {
 
 	page := 1
 	for {
+		if count > 500 {
+			count = 99999
+			break
+		}
 		resp, _, err := client.Users.ListFollowers(context.Background(), user, &github.ListOptions{Page: page, PerPage: 100})
 		if err == nil {
 			l := len(resp)
@@ -148,4 +156,37 @@ func getFollowerCounts(client *github.Client, user string) int {
 	}
 
 	return count
+}
+
+func getContributorsCount(client *github.Client, owner, repo string) int {
+	count := 0
+	page := 1
+	for {
+		options := new(github.ListContributorsOptions)
+		options.Anon = "false"
+		options.Page = page
+		options.PerPage = 100
+		if count > 500 {
+			logger.Println("contributors are more than 500")
+			// NOTE
+			// if number or contributors are more than 500
+			// then it is definite the repo is trustable
+			count = 99999 // setting to maximum count; so that check can be passed
+			break
+		}
+		resp, _, err := client.Repositories.ListContributors(context.Background(), owner, repo, options)
+
+		if err == nil {
+			c := len(resp)
+			count += c
+			if c < 100 {
+				break
+			}
+			page += 1
+		}
+
+	}
+
+	return count
+
 }
