44f1f5c9c97473d27cbe29bb73c9d429701a07fa
h0x0er <jatink843@protonmail.com>
2022-07-13 16:06:08 +0530

    added flags

--- main/inspector.go
+++ main/inspector.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/h0x0er/ossinspector"
 )
+
+type Options struct {
+	policy   string
+	verbose  bool
+	respType string
+	repo     string
+}
 
 func init() {
 	log.SetOutput(os.Stdout)
@@ -13,15 +23,23 @@ func init() {
 }
 
 func main() {
+	options := new(Options)
+	flag.StringVar(&options.policy, "policy", "policy.yml", "location of policy.yml file")
+	flag.StringVar(&options.policy, "p", "policy.yml", "location of policy.yml file")
+	flag.BoolVar(&options.verbose, "verbose", false, "enable verbose response")
+	flag.BoolVar(&options.verbose, "v", false, "enable verbose response")
+	flag.StringVar(&options.repo, "repo", "facebook/react", "repository to inspect")
+	flag.StringVar(&options.repo, "r", "facebook/react", "repository to inspect")
 
-	pth := os.Args[1]
-	policy, err := ossinspector.NewPolicy(pth)
+	flag.Parse()
+	policy, err := ossinspector.NewPolicy(options.policy)
 	if err != nil {
 		log.Fatalf("Unable to parse policy file\n%s\n", err)
 	}
 	log.Println("policy: ", policy)
-	info, _ := ossinspector.FetchRepoInfo("step-security", "harden-runner")
+	repo := strings.Split(options.repo, "/")
+	info, _ := ossinspector.FetchRepoInfo(repo[0], repo[1])
 
 	ok := ossinspector.Validate(policy, info)
-	log.Printf("policy followed: %v\n", ok)
+	fmt.Printf("policy followed: %v\n", ok)
 }
