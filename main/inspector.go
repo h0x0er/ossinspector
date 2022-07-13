aaccdaadd36a47b576f4547730c9bdf770c05c88
h0x0er <jatink843@protonmail.com>
2022-07-13 19:28:27 +0530

    added flags

--- main/inspector.go
+++ main/inspector.go
@@ -17,6 +17,8 @@ type Options struct {
 	repo     string
 }
 
+var logger *ossinspector.Logger
+
 func init() {
 	log.SetOutput(os.Stdout)
 	log.SetPrefix("oss-inspector: ")
@@ -24,22 +26,51 @@ func init() {
 
 func main() {
 	options := new(Options)
+
 	flag.StringVar(&options.policy, "policy", "policy.yml", "location of policy.yml file")
 	flag.StringVar(&options.policy, "p", "policy.yml", "location of policy.yml file")
 	flag.BoolVar(&options.verbose, "verbose", false, "enable verbose response")
 	flag.BoolVar(&options.verbose, "v", false, "enable verbose response")
 	flag.StringVar(&options.repo, "repo", "facebook/react", "repository to inspect")
 	flag.StringVar(&options.repo, "r", "facebook/react", "repository to inspect")
+	flag.StringVar(&options.respType, "resp", "", "format of response\nsupported formats are json, yaml; by default boolean value is returned.")
+	flag.StringVar(&options.respType, "rt", "", "format of response")
 
 	flag.Parse()
+
+	logger = ossinspector.NewLogger(options.verbose)
+
 	policy, err := ossinspector.NewPolicy(options.policy)
 	if err != nil {
-		log.Fatalf("Unable to parse policy file\n%s\n", err)
-	}
-	log.Println("policy: ", policy)
-	repo := strings.Split(options.repo, "/")
-	info, _ := ossinspector.FetchRepoInfo(repo[0], repo[1])
+		logger.Printf("Unable to parse policy file\n%s\n", err)
+		fmt.Printf("err: %v\n", err)
+		os.Exit(-1)
 
-	ok := ossinspector.Validate(policy, info)
-	fmt.Printf("policy followed: %v\n", ok)
+	}
+	logger.Println("policy: ", policy)
+
+	repo := strings.Split(options.repo, "/")
+	logger.Printf("Performing policy check on %s/%s", repo[0], repo[1])
+	info, err := ossinspector.FetchRepoInfo(repo[0], repo[1])
+	if err != nil {
+		logger.Printf("%v", err)
+		fmt.Printf("err: %v\n", err)
+		os.Exit(-1)
+	}
+
+	ok, resp := ossinspector.Validate(policy, info)
+	switch options.respType {
+	case "json":
+		fmt.Println(resp.ToJson())
+
+	case "yaml":
+		fmt.Println(resp.ToYaml())
+
+	case "yml":
+		fmt.Println(resp.ToYaml())
+
+	default:
+		fmt.Println(ok)
+
+	}
 }
